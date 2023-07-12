e44a40fbc6eaca6a3e30280709b944844e880c1e
Herminio Neto <herminio_neto@outlook.com>
2023-07-12 16:57:29 -0300

    add coyote jump timer

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,15 +1,14 @@
 extends CharacterBody2D
 
-
 const SPEED = 100.0
 const ACCELERATION = 800.0
 const FRICTION = 1000.0
 const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer: Timer = $CoyoteJumpTimer
 
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
@@ -18,17 +17,21 @@ func _physics_process(delta: float) -> void:
 	handle_acceleration(direction, delta)
 	apply_friction(direction, delta)
 	update_animations(direction)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var left_the_ledge = was_on_floor and not is_on_floor() and velocity.y > 0
+	if left_the_ledge:
+		coyote_jump_timer.start()
 	
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
 func handle_jump():
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if not is_on_floor():
 		if Input.is_action_just_released("jump") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 			
